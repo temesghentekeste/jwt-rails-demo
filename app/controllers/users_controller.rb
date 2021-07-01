7c9d5e126fba8806883895df74a075b14cc204d4
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-01 23:45:01 +0300

    Implement create user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def create
+    user = User.new(
+        name: params[:name], 
+        username: params[:username], 
+        password: params[:password], 
+        email: params[:email]
+      )
+
+    if user.save
+      render json: user
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
   end
+
+  private 
+  
+ 
 end
