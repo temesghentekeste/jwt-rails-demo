059120efc9872f20eab8eeef6a506e0dd6d362f5
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-02 02:10:41 +0300

    Amend bug on authentication create controller action

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,15 +2,12 @@ class AuthenticationController < ApplicationController
     def login
         user = User.find_by(username: params[:username])
 
-        if(!user)
-            render json: {"error": "Invalid username"}
-        end
-
-        if user.authenticate(params[:password])
+        if !user
+            render json: { "error": "Invalid username"}, status: :unauthorized
+        elsif user.authenticate(params[:password])
             render json: { "message": "Correct password"}
         else
-            render json: { "message": "Wrong passord"}
-
+            render json: { "message": "Wrong passord"}, status: :unauthorized
         end
     end
 end
