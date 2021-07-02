f82b742139c6ced47051595ef04de5d207a5595e
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-02 21:58:25 +0300

    Find logged in user using decoded token

--- app/controllers/balloons_controller.rb
+++ app/controllers/balloons_controller.rb
@@ -11,7 +11,11 @@ class BalloonsController < ApplicationController
         authorized_headers = request.headers[:authorization]
 
         if(authorized_headers)
-            token = authorized_headers.split(" "[1])
+            token = authorized_headers.split(" ")[1]
+            secret_key = Rails.application.secrets.secret_key_base[0]
+            decoded_token = JWT.decode(token, secret_key)
+
+            user = User.find(decoded_token[0]["id"])
             if balloon.save 
                 render json: {balloon: balloon}, status: :created
             else
