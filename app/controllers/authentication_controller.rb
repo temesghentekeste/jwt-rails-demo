4c81f4693ab5ee9c813f32e8b3415cbf8eef7fa4
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-14 21:05:31 +0300

    Add debug logs

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,12 +1,12 @@
 class AuthenticationController < ApplicationController
     def login
         user = User.find_by(username: params[:username])
+        puts "****************************************"
+        p Rails.application.secrets.secret_key_base[0]
+        puts "****************************************"
 
         if !user
             render json: { "error": "Invalid username"}, status: :unauthorized
-            puts "****************************************"
-            p Rails.application.secrets.secret_key_base[0]
-            puts "****************************************"
         elsif user.authenticate(params[:password])
             secret_key = Rails.application.secrets.secret_key_base[0]
             puts "****************************************"
